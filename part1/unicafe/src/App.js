ea410ca879483580a74044846b434fe346c98301
jashn117 <jashn.singh117@gmail.com>
2022-12-28 20:45:45 +0530

    part1 exercise 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,13 +1,43 @@
-import { setState } from 'react';
+import { useState } from 'react';
+
+function Feedback({incrementGood, incrementBad, incrementNeutral}) {
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button type="button" onClick={incrementGood}>good</button>
+      <button type="button" onClick={incrementNeutral}>neutral</button>
+      <button type="button" onClick={incrementBad}>bad</button>
+    </div>
+  );
+}
+
+function Stats({good, bad, neutral}) {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
+}
 
 function App() {
-  const [good, setGood] = setState(0);
-  const [bad, setBad] = setState(0);
-  const [neutral, setNeutral] = setState(0);
+  const [good, setGood] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+
+  const incrementReview = (func, count) => {
+    return () => func(count + 1);
+  };
 
   return (
     <div className="App">
-      code here
+      <Feedback
+        incrementGood={incrementReview(setGood, good)} 
+        incrementBad={incrementReview(setBad, bad)}
+        incrementNeutral={incrementReview(setNeutral, neutral)} />
+      <Stats good={good} bad={bad} neutral={neutral} />
     </div>
   );
 }
