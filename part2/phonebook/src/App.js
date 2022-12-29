0814de761beb6d909e8f62cbafbf3cf890ff0a39
jashn117 <jashn.singh117@gmail.com>
2022-12-29 16:12:08 +0530

    part2 exercises 2.6-2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,22 +2,74 @@ import { useState } from 'react';
 
 function App() {
   const [people, setPeople] = useState([
-    { name: 'Arto Hellas' },
+    { name: 'Arto Hellas', number: '405-9765-203' },
   ]);
   const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const updateName = (evt) => {
+    setName(evt.target.value);
+  };
+
+  const updateNumber = (evt) => {
+    setNumber(evt.target.value);
+  };
+
+  const updateFilter = (evt) => {
+    setFilter(evt.target.value);
+  }
+
+  const updatePeopleList = (evt) => {
+    evt.preventDefault();
+
+    const matchName = people.some(
+      (person) => person.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (matchName) {
+      alert(`${name} already exists!`);
+      return;
+    }
+
+    setPeople(
+      people.concat({ name, number })
+    );
+    setName('');
+    setNumber('');
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter names with: <input value={filter} onChange={updateFilter} />
+      </div>
+      <h2>add new people</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={name} onChange={updateName} placeholder="Jane Doe" />
         </div>
         <div>
-          <button type="submit">add</button>
+          number: <input value={number} onChange={updateNumber} placeholder="355-5555-505" />
+        </div>
+        <div>
+          <button type="submit" onClick={updatePeopleList}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <table>
+        <tbody>
+          {people
+            .filter(({name, number}) => name.toLowerCase().includes(filter))
+            .map(({name, number}) => (
+              <tr key={name + number}>
+                <td>{name}</td><td>{number}</td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
